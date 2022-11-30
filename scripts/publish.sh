7e137507bb5779715c789a4667dc6fd68c868602
chrisorz <imchrisorz@gmail.com>
2022-12-01 01:07:16 +0800

    ci: 新增publish脚本

---
+++ scripts/publish.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+current_branch_name=$(git symbolic-ref --short HEAD)
+
+npm version patch
+
+ver=$(cat ./package.json | grep -e '"version":' | tr -cd "[0-9].")
+
+git add .
+
+git commit -m "ci: 应用程序更新,当前版本:${ver}"
+
+git push origin "origin/${current_branch_name}"
+
+
+image_name="imchrisorz/goods-store-service"
+
+docker build -t "${image_name}:${ver}" .
+
+docker tag "${image_name}:latest"
+
+docker push "${image_name}:latest"
