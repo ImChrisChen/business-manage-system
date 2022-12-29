e5467b2ac029dd8c25a41a050cc92c8115461758
imchrisorz <imchrisorz@gmail.com>
2022-12-29 22:36:37 +0800

    ci: github actions

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,18 +1,9 @@
 #!/usr/bin/env bash
 
-current_branch_name=$(git symbolic-ref --short HEAD)
-
-npm version patch
-
-ver=$(cat ./package.json | grep -e '"version":' | tr -cd "[0-9].")
-
-git add .
-
-git commit -m "ci: 版本发布,当前版本:${ver}"
-
-git push origin "origin/${current_branch_name}"
-
 image_name="imchrisorz/business-manage-system"
+
+# 当前路径是脚本执行路径
+ver=$(cat ./package.json | grep -e '"version":' | tr -cd "[0-9].")
 
 docker build -t "${image_name}:${ver}" .
 
